72d5cf4ffcfa5006a73a2df8b9e2ca9b975281ec
Alfred Kutengule <akutengule@uwc.ac.za>
2021-05-27 04:01:06 +0200

    Seeded Data and Added Entities

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,8 +1,9 @@
 
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Core.Data
+namespace Infrastructure.Data
 {
     public class StoreContext : DbContext
     {
@@ -11,5 +12,14 @@ namespace Core.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        
     }
 }
